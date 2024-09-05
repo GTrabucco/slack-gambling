05672e0949a1132e01138c1e2a8bbdc596d792b7
GTrabuccoBEP <giulian.trabucco@bepproduce.com>
2024-09-05 12:56:16 -0400

    removed unused imports

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { useAuth } from "../../hooks/AuthProvider";
 import { Container } from 'react-bootstrap';
 
 const Dashboard = () => {
-  const auth = useAuth();
   return (
     <Container>
         <h1>Dashboard</h1>
