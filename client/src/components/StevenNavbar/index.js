4bfb74f5a55fd3574380267124ac6be9a5f1f114
GTrabuccoBEP <giulian.trabucco@bepproduce.com>
2025-01-18 21:53:38 -0500

    temp removing week picks

--- client/src/components/StevenNavbar/index.js
+++ client/src/components/StevenNavbar/index.js
@@ -18,7 +18,7 @@ const StevenNavbar = () => {
 
     const menuLinks = [
         { path: "/dashboard", label: "Dashboard" },
-        { path: "/weekpicks", label: "Week Picks" },
+        //{ path: "/weekpicks", label: "Week Picks" },
         { path: "/standings", label: "Standings" },
         { path: "/pickhistory", label: "Pick History" },
         { path: "/statistics", label: "Statistics" },
--- processpicks.py
+++ processpicks.py
@@ -1,7 +1,7 @@
 import requests
 
 def process_picks(season, week, picks):
-    week_type = 2  # 2 is regular season
+    week_type = 3  # 2 is regular season
     boxscores_url = f"https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/{season}/types/{week_type}/weeks/{week}/events?lang=en&region=us"
     game_summary_url = f"https://site.api.espn.com/apis/site/v2/sports/football/nfl/summary?event="
     event_ids = []
@@ -64,3 +64,5 @@ def process_picks(season, week, picks):
             raise ValueError(f"Error Processing Pick: {pick}")
 
     return picks
+
+
