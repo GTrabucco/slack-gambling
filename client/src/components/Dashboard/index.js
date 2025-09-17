e4c86a15636db66fdec20d6ca516771729522fd3
GTrabuccoBEP <giulian.trabucco@bepproduce.com>
2025-09-16 23:19:58 -0400

    Changes

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -424,7 +424,7 @@ const Dashboard = () => {
                         }
                       </b>
                     </Col>
-                    <Col style={{marginTop: 10, marginBottom: 10, marginLeft: 65}}>
+                    <Col style={{marginTop: 10, marginBottom: 10}}>
                       <span
                         onClick={()=>{ 
                           setShowStevenInfo(true)
